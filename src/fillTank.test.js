977c31fb16b0f574a9e9f26b56cff6e39f0bf12c
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-06-17 06:51:06 +0100

    pr changes

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-describe('fillTank', () => {
-  const { fillTank } = require('./fillTank');
+describe("fillTank", () => {
+  const { fillTank } = require("./fillTank");
 
-  it('should be declared', () => {
+  it("should be declared", () => {
     expect(fillTank).toBeDefined();
   });
 
-  it('should fill maxTankCapacity without amount', () => {
+  it("should fill maxTankCapacity without amount", () => {
     const customer = {
       money: 500,
       vehicle: {
@@ -38,7 +38,7 @@ describe('fillTank', () => {
     expect(customer.vehicle.fuelRemains).toBe(40);
   });
 
-  it(`should fill only the amount, the customer can afford`, () => {
+  it(`should fill only the amount the customer can afford`, () => {
     const customer = {
       money: 200,
       vehicle: {
@@ -51,7 +51,7 @@ describe('fillTank', () => {
 
     fillTank(customer, fuelPrice, amount);
 
-    expect(customer.vehicle.fuelRemains).toBe(8);
+    expect(customer.vehicle.fuelRemains).toBe(28);
   });
 
   it(`should round floor filled amount to decimals`, () => {
